aa06684e0d4c7e24d6ff4a3361f2574bcbabb8b2
Bhagyashri Patil <bhagyashriyogeshpatil@gmail.com>
2024-04-18 10:41:37 +0000

    Add variables and pettern to implement logic for TicTacToe game. Implement box clicking logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,34 @@ document.getElementById("user").addEventListener("keydown", function (event) {
         checkUsername();
     }
 });
+
+/**
+ * Set variables and win pattern combinations for Tic Tac Toe game screen 
+ */
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
+
+// 2D Array of win Patterns
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+/**
+ *  Logic of Tic Tac Toe game
+ */
+console.log("boxes", boxes);
+
+for (let i = 0; i < boxes.length; i++) {
+    console.log("Inside", boxes.length)
+    boxes[i].addEventListener("click", function() {
+        console.log("box:  ", boxes[i]);
+        console.log("Clicked");
+    });
+}
